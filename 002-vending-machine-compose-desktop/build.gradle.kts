c732b471dbdf8b535fe65447dbf9adde707bafc6
Philip Z <hello@teaql.io>
2026-06-15 00:58:20 +0800

    build: bump teaql dependencies to 1.512-RELEASE

--- 002-vending-machine-compose-desktop/build.gradle.kts
+++ 002-vending-machine-compose-desktop/build.gradle.kts
@@ -20,11 +20,11 @@ dependencies {
     implementation(compose.desktop.currentOs)
     
     // Depend on our generated TeaQL core and local app logic!
-    implementation("io.teaql:teaql-core:1.511-RELEASE")
-    implementation("io.teaql:teaql-sqlite:1.511-RELEASE")
+    implementation("io.teaql:teaql-core:1.512-RELEASE")
+    implementation("io.teaql:teaql-sqlite:1.512-RELEASE")
     implementation("org.xerial:sqlite-jdbc:3.45.2.0")
-    implementation("io.teaql:teaql-provider-jdbc:1.511-RELEASE")
-    implementation("io.teaql:teaql-data-service-sql:1.511-RELEASE")
+    implementation("io.teaql:teaql-provider-jdbc:1.512-RELEASE")
+    implementation("io.teaql:teaql-data-service-sql:1.512-RELEASE")
 }
 
 sourceSets {
